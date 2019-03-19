914380f9cbba7f53c95f131600f7d0f86f7a5501
Col Perks <wild.fauve@gmail.com>
2019-03-19 13:39:26 +1300

    Fix enum NET_STATUS

--- lib/foucault/net_response_value.rb
+++ lib/foucault/net_response_value.rb
@@ -6,8 +6,9 @@ module Foucault
     OK                   = :ok
     UNAUTHORISED         = :unauthorised
     SYSTEM_FAILURE       = :system_failure
-    UNPROCESSABLE_ENTITY = :unprcessable_entity
-    NET_STATUS       = Types::Strict::Symbol.enum(OK, FAIL, UNAUTHORISED, SYSTEM_FAILURE)
+    UNPROCESSABLE_ENTITY = :unprocessable_entity
+    
+    NET_STATUS           = Types::Strict::Symbol.enum(OK, FAIL, UNAUTHORISED, SYSTEM_FAILURE, UNPROCESSABLE_ENTITY)
 
     attribute :status,          NET_STATUS
     attribute :code,            Types::Integer.optional
--- lib/foucault/version.rb
+++ lib/foucault/version.rb
@@ -1,3 +1,3 @@
 module Foucault
-  VERSION = "0.1.19"
+  VERSION = "0.1.20"
 end
